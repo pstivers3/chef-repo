9d8b7f03dcef2ad244b20c4693f44dd55622242c
pstivers3 <pstivers3@comcast.net>
2014-07-04 11:32:07 -0700

    test edit

--- cookbooks/boo/recipes/boofile.rb
+++ cookbooks/boo/recipes/boofile.rb
@@ -3,6 +3,7 @@ bash "recipe test" do
   cwd "/root"
   code <<-EOH
   echo "recipe test run on      $(date)" >> /tmp/chef_recipe_test.txt
+  echo "boo!      $(date)" >> /tmp/chef_recipe_test.txt
   EOH
 end
 
